da9c2ddfe28ad0164b4a23ec1f8db3d025a94a0c
geethikagrace <geethika2219@gmail.com>
2023-05-02 22:10:09 -0700

    sample1

--- 03-loops/for-each/main.tf
+++ 03-loops/for-each/main.tf
@@ -2,6 +2,7 @@
 resource "null_resource"  "fruits" {
 
   for_each = var.fruits
+
   provisioner "local-exec" {
     command = "echo Fruit Name -  ${each.key} - ${each.value}"
     //command = "echo   ${length(var.fruits)}"
@@ -13,5 +14,33 @@ variable "fruits" {
     apple =10
     orange=100
     banana=200
+  }
+}
+
+resource "null_resource"  "fruits2" {
+
+  for_each = var.fruits2
+
+  provisioner "local-exec" {
+    command = "echo Fruit Name -  ${each.key} - ${each.value["count"]}"
+    //command = "echo   ${length(var.fruits)}"
+  }
+}
+
+variable "fruits2" {
+  default = {
+    apple {
+      name =apple
+      count=10
+  }
+    orange {
+      name = orange
+      count= 100
+    }
+
+    banana {
+      name = banana
+      count = 200
+    }
   }
 }
