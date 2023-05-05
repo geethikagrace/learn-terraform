333340e2aa353a0623e02ad23bb6c04ac5ee2340
geethikagrace <geethika2219@gmail.com>
2023-05-04 20:47:00 -0700

    env test

--- 04-funtions/main.tf
+++ 04-funtions/main.tf
@@ -7,23 +7,23 @@ output "sample" {
   value = upper(var.sample)
 }
 
-#variable "sample1" {
-#  default = ["abc", "xyz"]
-#}
-#
-#variable "sample2" {
-#  default = {
-#    xyz = 100
-#    abc = 200
-#  }
-#}
-#
-### Print first value like var.sample1[0] with a function
-#
-#output "sample1" {
-#  value = element(var.sample1, 0)
-#}
-#
-#output "sample2" {
-#  value = lookup(var.sample2, "a1", "dummy")
-#}
+variable "sample1" {
+ default = ["abc", "xyz"]
+}
+
+variable "sample2" {
+  default = {
+    xyz = 100
+    abc = 200
+  }
+}
+
+## Print first value like var.sample1[0] with a function
+
+output "sample1" {
+  value = element(var.sample1, 0)
+}
+
+output "sample2" {
+  value = lookup(var.sample2, "a1", "dummy")
+}
