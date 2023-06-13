d0f8d6f34b3d821e3625644060b55a4838c90a00
geethika <geethilka2219@gmail.com>
2023-06-13 03:51:44 -0700

    demo terraform

--- ex1/module/vars.tf
+++ ex1/module/vars.tf
@@ -1,8 +1,8 @@
-variable "env" {}
-variable "instance_type" {}
-variable "component_name" {}
-variable "password" {}
-variable "provisioner" {
-  default = false
-}
-variable "app_type" {}
+#variable "env" {}
+#variable "instance_type" {}
+#variable "component_name" {}
+#variable "password" {}
+#variable "provisioner" {
+#  default = false
+#}
+#variable "app_type" {}
